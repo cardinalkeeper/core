62dfc6a32119cddc86ae9d7339b46fd1e8534d5e
Khusamov Sukhrob <khusamov@yandex.ru>
2015-11-12 21:08:30 +0300

    Добавлена заготовка главного файла модуля

---
+++ index.js
@@ -0,0 +1,36 @@
+
+"use strict";
+
+// https://github.com/jsymfony/autoload
+let autoload = require("jsymfony-autoload");
+
+
+
+
+/* global CardinalKeeper */
+autoload.register("CardinalKeeper", __dirname + "/library");
+
+
+
+var cardinalkeeper = new CardinalKeeper.Application();
+
+
+
+
+
+
+
+
+
+
+// старая версия - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+var cardinalkeeper = new CardinalKeeper.Application();
+
+cardinalkeeper.publishFolder("/client", __dirname + "/client");
+
+if (fs.existsSync(__dirname + "/bower_components")) cardinalkeeper.publishFolder("/vendor", __dirname + "/bower_components");
+if (fs.existsSync("./bower_components")) cardinalkeeper.publishFolder("/vendor", "./bower_components");
+
+module.exports = cardinalkeeper;
