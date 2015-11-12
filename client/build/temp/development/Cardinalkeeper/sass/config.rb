bb8312cca79768b0dd0b48cec97671f186fb603c
Khusamov Sukhrob <khusamov@yandex.ru>
2015-11-12 21:06:53 +0300

    Добавлены первые файлы клиентской части

---
+++ client/build/temp/development/Cardinalkeeper/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\cardinalkeeper\sass\config.rb')
+cache_path = 'C:\@repositories\cardinalkeeper-core\client/build/.sass-cache'
---
+++ client/build/temp/development/Cardinalkeeper/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\@repositories\cardinalkeeper-core\client\cardinalkeeper\app.js
