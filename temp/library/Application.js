e5813e8fda61b43c3552fbf0cc9d5c245394861e
Khusamov Sukhrob <khusamov@yandex.ru>
2015-11-17 14:52:41 +0300

    Добавлена временная папка

---
+++ temp/library/Application.js
@@ -0,0 +1,223 @@
+
+"use strict";
+/* global CardinalKeeper */
+
+let autoload = require("jsymfony-autoload");
+
+/**
+ * Класс приложения.
+ * @class CardinalKeeper.Application
+ */
+
+var ini = require("multi-ini");
+var express = require("express");
+var bodyParser = require("body-parser"); // https://www.npmjs.com/package/body-parser
+require("express-resource"); // https://github.com/expressjs/express-resource
+
+module.exports = class Application {
+	
+	/**
+	 * Опции приложения.
+	 * @property {Object}
+	 */
+	get config() {
+		return this._config;
+	}
+	
+	/**
+	 * Адаптер основной базы данных приложения.
+	 * https://github.com/vitaly-t/pg-promise#usage
+	 * @property {pg-promise.Database}
+	 */
+	get database() {
+		return this._database;
+	}
+	
+	/**
+	 * HTTP-сервер от Node.js.
+	 * https://nodejs.org/api/http.html
+	 * @property {http.Server}
+	 */
+	get server() {
+		return this._server;
+	}
+	
+	/**
+	 * Express-приложение.
+	 * http://expressjs.com/4x/api.html#app
+	 * @property {express.Application}
+	 */
+	get express() {
+		return this._express;
+	}
+	
+	get client() {
+		return this._client;
+	}
+	
+	/**
+	 * Конструктор приложения.
+	 */
+	constructor() {
+		var me = this;
+		me._express = express();
+		me.express.use(bodyParser.json());
+		
+		// Создать клиент для базы данных PostgreSQL.
+		me._postgres = new CardinalKeeper.database.Postgres({
+			connect: function(client) {
+				console.log("Connected to database:\n", client.connectionParameters);
+			},
+			disconnect: function(client) {
+				console.log("Disconnecting from database:\n", client.connectionParameters);
+			},
+		});
+		
+		me._client = new CardinalKeeper.Client(me);
+		me.express.get("/", me._client.middleware);
+	}
+	
+	/**
+	 * Инициализация приложения.
+	 * Подключает конфигурационный файл приложения.
+	 * @param {String} configPath
+	 * @return {CardinalKeeper.Application}
+	 */
+	init(configPath) {
+		var me = this;
+		
+		// Подключить конфигурационный файл.
+		me._configFile = ini.read(configPath);
+		me._config = me.getConfigSection("application");
+		
+		// Добавляем в конфиг переменную path содержащую абсолютный путь к папке с конфигурационным файлом.
+		var mpath = require("path");
+		me._config.path = mpath.dirname(mpath.resolve(configPath));
+		
+		// Подключить основную базу данных.
+		me._database = me._postgres.createAdapter(me.config.database);
+		
+		// Подключить модули приложения.
+		me.config.module.forEach(path => {
+			me.module(path);
+		});
+		//me._config.cardinalkeeper.module.forEach(me.module.bind(me));
+		
+		return me;
+	}
+	
+	/**
+	 * Получить раздел основного конфигурационного файла приложения.
+	 * @param {String} section
+	 * @return {Object}
+	 */
+	getConfigSection(section) {
+		
+		let result = this._configFile[section] || {};
+		
+		result.isDefined = function(path) {
+			path = path.split(".");
+			let current = result;
+			let defined = true;
+			path.forEach(item => {
+				if (item in current) {
+					current = current[item];
+				} else {
+					defined = false;
+					return false;
+				}
+			});
+			return defined;
+		};
+		
+		result.get = function(path, defaultValue) {
+			let value = result;
+			path = path.split(".");
+			path.every(item => {
+				if (item in value) {
+					value = value[item];
+				} else {
+					value = defaultValue;
+					return false;
+				}
+				return true;
+			});
+			return value;
+		};
+		
+		return result;
+	}
+	
+	/**
+	 * Подключить модуль.
+	 * Внимание, относительные пути (начинающиеся с ./) отсчитываются от месторасположения конфига.
+	 * @param {String} path
+	 * @return {CardinalKeeper.Application}
+	 */
+	module(path) {
+		var me = this;
+		path = path.replace(/^.\//, me.config.path + "/");
+		path = path.replace(/^..\//, me.config.path + "/../");
+		
+		path = require("path").normalize(path);
+		
+		let Module = require(path);
+		let module = new Module(me);
+		
+		//console.log("Module", require("path").dirname(require.resolve(path)));
+		
+		// Регистрируем пространство имен модуля.
+		//autoload.register("CardinalKeeper.module." + module.name, require("path").dirname(path));
+		
+		//autoload.register("CardinalKeeper.module." + module.name, module._homedir); // https://toster.ru/q/261490
+		autoload.register("CardinalKeeper.module." + module.name, require("path").dirname(require.resolve(path))); // https://toster.ru/q/261490
+		
+		
+		//console.log("CardinalKeeper.module." + module.name, require("path").dirname(path));
+		
+		
+		me.express.use("/api/" + module.name, module.express);
+		me.client.use(module.client);
+		return me;
+	}
+
+	/**
+	 * Опубликовать папку в вебе.
+	 * @param {String} [webPath]
+	 * @param {String} folder
+	 * @return {CardinalKeeper.Application}
+	 */
+	publishFolder(webPath, folder) {
+		var me = this;
+		if (folder) {
+			me.express.use(webPath, express.static(folder));
+		} else {
+			me.express.use(express.static(webPath));
+		}
+	}
+	
+	/**
+	 * Создать веб-сервер.
+	 * @return {http.Server}
+	 */
+	listen() {
+		return this.express.listen.apply(this.express, arguments);
+	}
+	
+	/**
+	 * Запустить приложение.
+	 * @param {String} [configPath] Путь к конфигурационному файлу.
+	 * @return {CardinalKeeper.Application}
+	 */
+	run(configPath) {
+		var me = this;
+		if (configPath) me.init(configPath);
+		me._server = me.listen(me.config.port, function () {
+			var port = me.server.address().port;
+			console.log("Cardinal Keeper: Application started.");
+			console.log("Cardinal Keeper: Listening at port %s.", port);
+		});
+		return me;
+	}
+	
+};
---
+++ temp/library/Client.js
@@ -0,0 +1,124 @@
+
+"use strict";
+/* global CardinalKeeper */
+
+var pretty = require("pretty");
+
+/**
+ * Класс клиентского приложения.
+ * @class CardinalKeeper.Client
+ */
+
+module.exports = class Client {
+
+	/**
+	 * Express-middleware.
+	 * @property {Function}
+	 */
+	get middleware() {
+		return this._middleware;
+	}
+	
+	/**
+	 * Подключить клиентскую часть модуля приложения.
+	 * @param {CardinalKeeper.module.Client} client
+	 */
+	use(client) {
+		var me = this;
+		me._modules.push(client);
+	}
+	
+	/**
+	 * Конструктор приложения.
+	 */
+	constructor(application) {
+		var me = this;
+		
+		me._application = application;
+		me._modules = [];
+		
+		me._middleware = function(request, response) {
+			
+			var appConfig = me._application.config;
+			
+			// HEAD
+			let head = [];
+			
+			head.push('<meta charset="utf-8">');
+			head.push('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">');
+			
+			head.push('<link href="/client/favicon.gif" rel="shortcut icon" type="image/x-icon">');
+			
+			// PACE
+			let paceOptions = JSON.stringify({
+				restartOnPushState: false,
+				restartOnRequestAfter: false
+			});
+			head.push('<link href="/vendor/pace/themes/blue/pace-theme-center-simple.css" rel="stylesheet" />');
+			head.push('<style type="text/css">.pace { height: 15px; }</style>');
+			head.push(`<script data-pace-options='${paceOptions}' src='/vendor/pace/pace.js'></script>`);
+			
+			// Sencha Ext JS
+			let extjsPath = "/vendor/khusamov-sencha-extjs";
+			if (appConfig.get("sencha.extjs.source.type") == "localhost") {
+				extjsPath = "http://localhost/ext-" + appConfig.get("sencha.extjs.source.version", "5.1.1");
+			}
+			let theme = appConfig.get("sencha.extjs.theme", "crisp");
+			let debug = appConfig.get("sencha.extjs.debug") ? "-debug" : "";
+			let locale = appConfig.get("sencha.extjs.locale", "ru");
+			head.push(`<link href="${extjsPath}/build/packages/ext-theme-${theme}/build/resources/ext-theme-${theme}-all${debug}.css" rel="stylesheet">`);
+			head.push(`<script src="${extjsPath}/build/ext-all${debug}.js"></script>`);
+			head.push(`<script src="${extjsPath}/build/packages/ext-locale/build/ext-locale-${locale}${debug}.js"></script>`);
+			
+			// Убрать когда выйдет Хром версии 44
+			// http://javascript.ru/forum/extjs/56067-input-vniz-sekhali-vse.html
+			head.push('<style type="text/css">.x-form-text { display: inherit; }</style>');
+			
+			// Подключение библиотеки Khusamov Ext JS
+			head.push('<script src="/vendor/khusamov-extjs/packages/delegates.js"></script>');
+			head.push('<script>Ext.Loader.setPath("Khusamov", "/vendor/khusamov-extjs/src");</script>');
+			
+			
+			let controllers = ["Root"];
+			let namespaces = [];
+			let models = ["MainMenuItem"];
+			let stores = ["MainMenu"];
+			
+			me._modules.forEach(module => {
+				//namespaces.push(module.config.get("namespace")); // это не прокатит, так как module это объект класса CardinalKeeper.module.Client
+				// и в module.config нет метода get()
+				namespaces.push(module.config.namespace);
+				controllers = controllers.concat(module.controllers);
+			});
+			
+			// Подключение клиентского приложения
+			var appOptions = JSON.stringify({
+				title: appConfig.get("title", "Кардинал Кипер 2015"),
+				name: "CardinalKeeper",
+				appFolder: "/client",
+				extend: "CardinalKeeper.Application",
+				namespaces: namespaces,
+				controllers: controllers,
+				models: models,
+				stores: stores
+			});
+			head.push(`<script>Ext.application(${appOptions});</script>`);
+			
+			// HTML
+			let html = [];
+			
+			html.push("<head>");
+			html.push(head.join(""));
+			html.push("</head>");
+			html.push('<body></body>');
+			
+			let output = '<!DOCTYPE html><html lang="ru">' + html.join("") + "</html>";
+			
+			output = pretty(output);
+			
+			response.send(output);
+		};
+		
+	}
+	
+};
